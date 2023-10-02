22ffb4de9bad7e90eef377477172ee43c4fb18d8
Nadiia17 <nababiy@gmail.com>
2023-10-03 00:54:11 +0300

    upd-2

---
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -0,0 +1,11 @@
+export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
+  <ul>
+    {options.map(option => (
+      <li key={option}>
+        <button type="button" onClick={() => onLeaveFeedback(option)}>
+          {option}
+        </button>
+      </li>
+    ))}
+  </ul>
+);
